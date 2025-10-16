61a37fc33b2fda56b3e7a5df2ed916bdec45eb30
nayibe gomez <nayibe5u262@gmail.com>
2025-10-16 05:17:23 -0500

    Primer commit

--- core/config.py
+++ core/config.py
@@ -1,30 +1,30 @@
 from pydantic_settings import BaseSettings
-import os
-from dotenv import load_dotenv
-
-# librería en Python que permite cargar variables de entorno
-load_dotenv()
+from pydantic import Field
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "AVISENA"
     PROJECT_VERSION: str = "0.0.1"
     PROJECT_DESCRIPTION: str = "Aplicación para gestionar granjas avicolas"
 
-    # Configuración de la base de datos
-    DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: int = int(os.getenv("DB_PORT", "3306"))
-    DB_USER: str = os.getenv("DB_USER", "root")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "")
-    DB_NAME: str = os.getenv("DB_NAME", "")
+    DB_HOST: str = Field("localhost", env="DB_HOST")
+    DB_PORT: int = Field(3306, env="DB_PORT")
+    DB_USER: str = Field("root", env="DB_USER")
+    DB_PASSWORD: str = Field("", env="DB_PASSWORD")
+    DB_NAME: str = Field("", env="DB_NAME")
 
-    DATABASE_URL: str = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    
-    # Configuración JWT
-    jwt_secret: str = os.getenv("JWT_SECRET")
-    jwt_algorithm: str = os.getenv("JWT_ALGORITHM", "HS256")
-    jwt_access_token_expire_minutes: int = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+    jwt_secret: str = Field(..., env="JWT_SECRET")  # obligatorio
+    jwt_algorithm: str = Field("HS256", env="JWT_ALGORITHM")
+    jwt_access_token_expire_minutes: int = Field(30, env="JWT_ACCESS_TOKEN_EXPIRE_MINUTES")
 
     class Config:
         env_file = ".env"
+        env_file_encoding = 'utf-8'
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
 
 settings = Settings()
